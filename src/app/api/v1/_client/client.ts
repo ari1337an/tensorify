30455db620f209d690f2926f84b52a33913dfa1b
Md Sahadul Hasan Arian <ari1337an@gmail.com>
2025-07-04 18:27:08 +0600

    feat: fixed breadcrump

--- src/app/api/v1/_client/client.ts
+++ src/app/api/v1/_client/client.ts
@@ -159,24 +159,6 @@ export async function getOrganizationUsers(args: Parameters<typeof client.getOrg
 }
 
 
-export async function getPermissions(args: Parameters<typeof client.getPermissions.contract>[0]): Promise<ReturnType<typeof client.getPermissions.contract>> {
-  const dynamicClient = await getClientWithBaseUrl();
-  return await dynamicClient.getPermissions.contract(args);
-}
-
-
-export async function getProject(args: Parameters<typeof client.getProject.contract>[0]): Promise<ReturnType<typeof client.getProject.contract>> {
-  const dynamicClient = await getClientWithBaseUrl();
-  return await dynamicClient.getProject.contract(args);
-}
-
-
-export async function postProject(args: Parameters<typeof client.postProject.contract>[0]): Promise<ReturnType<typeof client.postProject.contract>> {
-  const dynamicClient = await getClientWithBaseUrl();
-  return await dynamicClient.postProject.contract(args);
-}
-
-
 export async function getRoles(args: Parameters<typeof client.getRoles.contract>[0]): Promise<ReturnType<typeof client.getRoles.contract>> {
   const dynamicClient = await getClientWithBaseUrl();
   return await dynamicClient.getRoles.contract(args);
@@ -192,6 +174,24 @@ export async function patchRole(args: Parameters<typeof client.patchRole.contrac
 export async function postRoles(args: Parameters<typeof client.postRoles.contract>[0]): Promise<ReturnType<typeof client.postRoles.contract>> {
   const dynamicClient = await getClientWithBaseUrl();
   return await dynamicClient.postRoles.contract(args);
+}
+
+
+export async function getPermissions(args: Parameters<typeof client.getPermissions.contract>[0]): Promise<ReturnType<typeof client.getPermissions.contract>> {
+  const dynamicClient = await getClientWithBaseUrl();
+  return await dynamicClient.getPermissions.contract(args);
+}
+
+
+export async function getProject(args: Parameters<typeof client.getProject.contract>[0]): Promise<ReturnType<typeof client.getProject.contract>> {
+  const dynamicClient = await getClientWithBaseUrl();
+  return await dynamicClient.getProject.contract(args);
+}
+
+
+export async function postProject(args: Parameters<typeof client.postProject.contract>[0]): Promise<ReturnType<typeof client.postProject.contract>> {
+  const dynamicClient = await getClientWithBaseUrl();
+  return await dynamicClient.postProject.contract(args);
 }
 
 
--- src/app/api/v1/_contracts/index.ts
+++ src/app/api/v1/_contracts/index.ts
@@ -10,16 +10,16 @@ import { contract as onboardingQuestionsContract, action as onboardingQuestionsA
 import { contract as onboardingSetupContract, action as onboardingSetupAction } from "./onboarding/onboardingSetup";
 import { contract as getOrganizationContract, action as getOrganizationAction } from "./organization/getOrganization";
 import { contract as getOrganizationUsersContract, action as getOrganizationUsersAction } from "./organization/getOrganizationUsers";
-import { contract as getTeamContract, action as getTeamAction } from "./team/getTeam";
-import { contract as postTeamContract, action as postTeamAction } from "./team/postTeam";
 import { contract as getPermissionsContract, action as getPermissionsAction } from "./permissions/getPermissions";
-import { contract as getProjectContract, action as getProjectAction } from "./project/getProject";
-import { contract as postProjectContract, action as postProjectAction } from "./project/postProject";
-import { contract as getUserRoleContract, action as getUserRoleAction } from "./user-roles/getUserRole";
-import { contract as postUserRoleContract, action as postUserRoleAction } from "./user-roles/postUserRole";
 import { contract as getRolesContract, action as getRolesAction } from "./roles/getRoles";
 import { contract as patchRoleContract, action as patchRoleAction } from "./roles/patchRole";
 import { contract as postRolesContract, action as postRolesAction } from "./roles/postRoles";
+import { contract as getProjectContract, action as getProjectAction } from "./project/getProject";
+import { contract as postProjectContract, action as postProjectAction } from "./project/postProject";
+import { contract as getTeamContract, action as getTeamAction } from "./team/getTeam";
+import { contract as postTeamContract, action as postTeamAction } from "./team/postTeam";
+import { contract as getUserRoleContract, action as getUserRoleAction } from "./user-roles/getUserRole";
+import { contract as postUserRoleContract, action as postUserRoleAction } from "./user-roles/postUserRole";
 import { contract as getWorkflowContract, action as getWorkflowAction } from "./workflow/getWorkflow";
 import { contract as postWorkflowContract, action as postWorkflowAction } from "./workflow/postWorkflow";
 
@@ -33,16 +33,16 @@ export const contract = c.router({
   onboardingSetup: onboardingSetupContract,
   getOrganization: getOrganizationContract,
   getOrganizationUsers: getOrganizationUsersContract,
-  getTeam: getTeamContract,
-  postTeam: postTeamContract,
   getPermissions: getPermissionsContract,
-  getProject: getProjectContract,
-  postProject: postProjectContract,
-  getUserRole: getUserRoleContract,
-  postUserRole: postUserRoleContract,
   getRoles: getRolesContract,
   patchRole: patchRoleContract,
   postRoles: postRolesContract,
+  getProject: getProjectContract,
+  postProject: postProjectContract,
+  getTeam: getTeamContract,
+  postTeam: postTeamContract,
+  getUserRole: getUserRoleContract,
+  postUserRole: postUserRoleContract,
   getWorkflow: getWorkflowContract,
   postWorkflow: postWorkflowContract,
 });
@@ -57,16 +57,16 @@ export const appRouter = tsr.routerWithMiddleware(contract)<{
   onboardingSetup: onboardingSetupAction,
   getOrganization: getOrganizationAction,
   getOrganizationUsers: getOrganizationUsersAction,
-  getTeam: getTeamAction,
-  postTeam: postTeamAction,
   getPermissions: getPermissionsAction,
-  getProject: getProjectAction,
-  postProject: postProjectAction,
-  getUserRole: getUserRoleAction,
-  postUserRole: postUserRoleAction,
   getRoles: getRolesAction,
   patchRole: patchRoleAction,
   postRoles: postRolesAction,
+  getProject: getProjectAction,
+  postProject: postProjectAction,
+  getTeam: getTeamAction,
+  postTeam: postTeamAction,
+  getUserRole: getUserRoleAction,
+  postUserRole: postUserRoleAction,
   getWorkflow: getWorkflowAction,
   postWorkflow: postWorkflowAction,
 });
